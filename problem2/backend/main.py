11115e1fcc991b6ef01aa56235cb3dab012dd1fa
kunalbhatia-868 <kunalbhatia868@gmail.com>
2022-05-18 02:47:19 +0530

    backend completed

--- problem2/backend/main.py
+++ problem2/backend/main.py
@@ -1,18 +1,20 @@
 from flask import Flask
-import requests
+from flickrapi import FlickrAPI
+import json
 
 app=Flask(__name__)
 
 @app.route("/api/search/<keyword>")
 def search_images(keyword):
     api_key='3aae1e7e09869acf99331417f801ee1f'
-    url=f"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={api_key}&format=json&tags=horses"
-    response=requests.get(url)
-    print(response.json())
-    return "completed"
+    secret_key='d610c270a6c80069'
+    flickr = FlickrAPI(api_key, secret_key,format='parsed-json')
+    extras='url_s'
+    photoData = flickr.photos.search(text=keyword, per_page=25, extras=extras)
+    photos = photoData['photos']
+    photosJson=json.dumps(photos.get('photo'))
+    return photosJson
+    
 
 if __name__=='__main__':
     app.run()
-    
-    
-    # //https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3aae1e7e09869acf99331417f801ee1f&format=json&tags=bank
