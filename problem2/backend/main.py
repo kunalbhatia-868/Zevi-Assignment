98f967b6730d9356384fde9bf25bc7c63ea12e35
kunalbhatia-868 <kunalbhatia868@gmail.com>
2022-05-18 03:27:57 +0530

    frontend backend joined

--- problem2/backend/main.py
+++ problem2/backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flickrapi import FlickrAPI
 import json
+from flask_cors import cross_origin
 
 app=Flask(__name__)
 
-@app.route("/api/search/<keyword>")
+@app.route("/api/search/<keyword>", methods=["GET"])
+@cross_origin()
 def search_images(keyword):
     api_key='3aae1e7e09869acf99331417f801ee1f'
     secret_key='d610c270a6c80069'
@@ -12,8 +14,8 @@ def search_images(keyword):
     extras='url_s'
     photoData = flickr.photos.search(text=keyword, per_page=25, extras=extras)
     photos = photoData['photos']
-    photosJson=json.dumps(photos.get('photo'))
-    return photosJson
+    response=json.dumps(photos.get('photo'))
+    return response 
     
 
 if __name__=='__main__':
--- problem2/frontend/src/App.js
+++ problem2/frontend/src/App.js
@@ -1,12 +1,46 @@
+import { useState,useEffect } from "react";
 import "./App.css";
-import ImageSet from "./components/ImageSet/ImageSet";
-import Navbar from "./components/Navbar/Navbar";
+import ImageCard from "./components/ImageCard/ImageCard"
+
 
 function App() {
+	const [imageData,setImageData]=useState([])
+	const [keyword,setKeyword]=useState('')
+
+	useEffect(()=>{
+		fetch("api/search/horses")
+		.then(response=>response.json())
+		.then(data=>{
+				setImageData(data);
+			})
+	},[])
+
+	const handleChange=(e)=>{
+		setKeyword(e.target.value);
+	}
 	return (
 		<div className="App">
-			<Navbar />
-			<ImageSet />
+			<div className="main-navbar">
+				<div className="nav-title">My Gallery</div>
+				<div className="nav-searchbar">
+					<input
+						type="search"
+						name="search"
+						id=""
+						placeholder="photos, people, groups"
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<button className="button">Login</button>
+					<button className="button">Submit</button>
+				</div>
+			</div>
+			<div className="image-set">
+			{imageData.map(singleImageData=>{
+				return <ImageCard key={singleImageData.id} singleImageData={singleImageData}/>
+			})}
+			</div>
 		</div>
 	);
 }
